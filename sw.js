98d3f7cf8197df2f1a8e9e707f176be55f2c719c
F1Zero <135384971+F1Zero@users.noreply.github.com>
2026-08-20 12:00:16 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Service Worker - SIG Escuelas Yucatán (generado automáticamente)
+const CACHE = 'sig-escuelas-20260820114700';
+const ASSETS = ['./', './index.html', './manifest.webmanifest',
+  './icon-192.png', './icon-512.png', './icon-180.png', './icon-maskable-512.png'];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{}));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== location.origin) return; // mapas/StreetView externos van directo a la red
+  e.respondWith(
+    caches.match(req).then(hit => hit || fetch(req).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(req, copy));
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
